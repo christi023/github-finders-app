9171b291b285a0167478175b1d9770ce7f0d4598
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-18 16:07:47 +0200

    Error page added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+// style
+import {ErrorStyle } from '../components/styles/ErrorStyle';
+
 const Error = () => {
-  return <h2>error page</h2>;
+  return (    
+  <ErrorStyle>
+    <div>
+      <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to="/" className="btn">Back home</Link>
+    </div>
+  </ErrorStyle>
+ 
+  )
 };
-const Wrapper = styled.section`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-  background: var(--clr-primary-10);
-  text-align: center;
-  h1 {
-    font-size: 10rem;
-  }
-  h3 {
-    color: var(--clr-grey-3);
-    margin-bottom: 1.5rem;
-  }
-`;
+;
 export default Error;
