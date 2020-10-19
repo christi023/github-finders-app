f57bd2afcedff40f6d01e666c27ee6efd155ce34
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-19 16:07:24 +0200

    JS styles for User

---
+++ src/components/styles/UserStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const UserStyle = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  /* align-items: start; */
+`;
