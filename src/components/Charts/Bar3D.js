41056ae16a44589457b70370860898e1f0c89c09
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-21 15:05:09 +0200

    Bar 3d Chart created for most forked repos

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,50 @@
 import React from 'react';
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
 
-const Bar3D = () => {
-  return <div>chart</div>;
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  // STEP 3 - Creating the JSON object to store the chart configurations
+  const chartConfigs = {
+    type: 'bar3d', // The chart type
+    width: '100%', // Width of the chart
+    height: '350', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: 'Most Forked',
+        captionFontColor: '#102a42',
+        captionFontBold: 0,
+        captionFontSize: 20,
+        captionFont: 'Roboto',
+        baseFont: 'Open Sans',
+        baseFontSize: 16,
+        baseFontColor: '#617d98',
+        smartLineColor: '#617d98',
+        showShadow: 0,
+        showPlotBorder: 0,
+        yAxisName: 'Forks',
+        xAxisName: 'Repos',
+        yAxisNameFontSize: '16px',
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Bar3D;
+export default ChartComponent;
