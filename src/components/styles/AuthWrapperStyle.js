0ca7dc57082fe905493a722b1cbdf7975a10bb4c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-22 14:52:20 +0200

    JS styles for Pages Auth Wrapper

---
+++ src/components/styles/AuthWrapperStyle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+export const AuthWrapperStyle = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  img {
+    width: 150px;
+  }
+`;
