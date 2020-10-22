ad00c35e97b9e0ae13605380e44a45a8b2db161b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-22 15:09:37 +0200

    Setup user with followers and card

--- src/components/User.js
+++ src/components/User.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
+// components
 import Card from './Card';
 import Followers from './Followers';
+// style
+import { UserStyle } from './styles/UserStyle';
+
 const User = () => {
-  return <h2>search component</h2>;
+  return (
+    <section className="section">
+      <UserStyle className="section-center">
+        <Card></Card>
+        <Followers></Followers>
+      </UserStyle>
+    </section>
+  );
 };
-
-const Wrapper = styled.div`
-  padding-top: 2rem;
-  display: grid;
-  gap: 3rem 2rem;
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-  }
-  /* align-items: start; */
-`;
-
 export default User;
